2df157467a148d17708cc32d415bb7772786b54c
[VivekGarg1] <[vivekgarg020@gmail.com]>
2020-08-01 12:57:16 +0530

    code merge

--- src/main/java/com/home/springbootthymeleafdemo/model/User.java
+++ src/main/java/com/home/springbootthymeleafdemo/model/User.java
@@ -19,4 +19,36 @@ public class User {
     private String gender;
     private String profile;
     private Boolean married;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getProfile() {
+		return profile;
+	}
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	public Boolean getMarried() {
+		return married;
+	}
+	public void setMarried(Boolean married) {
+		this.married = married;
+	}
+    
+   
 }
